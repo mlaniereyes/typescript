c4f70470d71d2e4cf5350a0fbc737eac96d8ff5a
Melanie Reyes <mlaniereyes@gmail.com>
2021-07-02 21:34:44 -0500

    tutorial - types and annotations

---
+++ features/annotations/objects.ts
@@ -0,0 +1,19 @@
+const profile = {
+  name: 'melanie',
+  age: 106,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+};
+
+const { age }: {
+  age: number
+} = profile;
+
+const { 
+  coords: {lat, lng} 
+}: { coords: { lat: number; lng: number} } = profile;
